4bbfc4825e6e3978e09da33eb4929a6803855a99
KLERMi <mokoladele@gmail.com>
2026-02-03 17:33:32 +0100

    update task 1

---
+++ Giraffe/Girraffe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    //This is a single line comment becasue the slash' got no asterisks, and it is not wrapped in /* */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine(name);
+            int myNum = 15;
+            myNum = 20;
+            Console.WriteLine(myNum);
+            /*            Console.WriteLine("Hello World!");
+                        Console.WriteLine("44-4");
+                        Console.Write(" = ");
+                        Console.Write(44 - 4);*/
+        }
+    }
+}
